84767b173f940f29a9a5d38f0920e9255dfd04af
Andrew Garcia <garciaa38@gmail.com>
2023-10-27 15:27:00 -0400

    initial commit

---
+++ phases/02-practice-problems.js
@@ -0,0 +1,29 @@
+function anagrams(str1, str2) {
+  // Your code here
+}
+
+
+function commonElements(arr1, arr2) {
+  // Your code here
+}
+
+
+function duplicate(arr) {
+  // Your code here
+}
+
+
+function twoSum(nums, target) {
+  // Your code here
+  for (let i = 0; i < nums.length; i++) {
+    set.has(nums[i] + nums[i+1] === target)
+  }
+}
+
+
+function wordPattern(pattern, strings) {
+  // Your code here
+}
+
+
+module.exports = [anagrams, commonElements, duplicate, twoSum, wordPattern];
